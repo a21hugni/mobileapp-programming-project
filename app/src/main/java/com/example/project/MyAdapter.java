fa66282aa9e6c881f25c1cece797a6608e43d3fc
Hugo <a21hugni@student.his.se>
2022-05-23 23:30:11 +0200

    Lade till layoutinflater och holder för varje attribut

--- app/src/main/java/com/example/project/MyAdapter.java
+++ app/src/main/java/com/example/project/MyAdapter.java
@@ -1,5 +1,7 @@
 package com.example.project;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -16,16 +18,21 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.id.setText(verktyg.get(position).getId());
+        holder.name.setText(verktyg.get(position).getName());
+        holder.location.setText(verktyg.get(position).getLocation());
+        holder.company.setText(verktyg.get(position).getCompany());
+        holder.size.setText(String.valueOf(verktyg.get(position).getSize()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return verktyg.size();
     }
 }
