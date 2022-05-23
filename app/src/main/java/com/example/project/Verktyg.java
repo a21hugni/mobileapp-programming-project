057857f5311f89726fb3427a865e8ae1350a1fb5
Hugo <a21hugni@student.his.se>
2022-05-23 23:14:17 +0200

    Lade till getters och setters till alla attribut

--- app/src/main/java/com/example/project/Verktyg.java
+++ app/src/main/java/com/example/project/Verktyg.java
@@ -1,4 +1,59 @@
 package com.example.project;
 
 public class Verktyg {
+
+    private String id;
+    private String name;
+    private String location;
+    private String company;
+    private int size;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Verktyg(String id, String name, String location, String company, int size) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.company = company;
+        this.size = size;
+    }
+
 }
