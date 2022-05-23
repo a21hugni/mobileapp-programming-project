c1c3b3f38c4752bbfd7834b19766c9a46b1b1f79
Hugo <a21hugni@student.his.se>
2022-05-23 23:22:34 +0200

    Lade till variabler i MyViewHolder och kopplade dem till respektive item

--- app/src/main/java/com/example/project/MyViewHolder.java
+++ app/src/main/java/com/example/project/MyViewHolder.java
@@ -1,4 +1,24 @@
 package com.example.project;
 
-public class MyViewHolder {
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    public TextView id;
+    public TextView name;
+    public TextView location;
+    public TextView company;
+    public TextView size;
+
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+        id = itemView.findViewById(R.id.item_id);
+        name = itemView.findViewById(R.id.item_name);
+        location = itemView.findViewById(R.id.item_location);
+        company = itemView.findViewById(R.id.item_company);
+        size = itemView.findViewById(R.id.item_size);
+    }
 }
