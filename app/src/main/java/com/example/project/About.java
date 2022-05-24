e26aa4abebb713ad9b58bc7fd7f43784b90d573c
a21hugni <a21hugni@student.his.se>
2022-05-24 10:53:27 +0200

    Gjorde webview och index sida

--- app/src/main/java/com/example/project/About.java
+++ app/src/main/java/com/example/project/About.java
@@ -3,12 +3,21 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class About extends AppCompatActivity {
+
+    private WebView webview;
+
+
+    //public void showInterWebPage() {
+        //webview.loadUrl("file://android_assets/index.html");
+    //}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
     }
 }
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
